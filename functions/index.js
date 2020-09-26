d7329e920e3a95ad4d5aa2b6c9f1fbe9597195a9
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-09-26 21:20:29 +0200

    random generator example

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,12 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+
+//http request 1
+exports.randomNumber = functions.https.onRequest((request,response) =>{
+
+    const number = Math.round(Math.random()*100);
+    response.send(number.toString());
+})
