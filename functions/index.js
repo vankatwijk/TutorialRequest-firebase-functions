8eaed90841ddc2b4f1fe910980bd11649b261b63
Hendrikus van Katwijk <hendrikus.hpvk@gmail.com>
2020-09-30 21:59:52 +0200

    auth triggers

--- functions/index.js
+++ functions/index.js
@@ -10,23 +10,33 @@ const functions = require('firebase-functions');
 
 
 
-//http request 1
-exports.randomNumber = functions.https.onRequest((request,response) =>{
+// //http request 1
+// exports.randomNumber = functions.https.onRequest((request,response) =>{
 
-    const number = Math.round(Math.random()*100);
-    response.send(number.toString());
+//     const number = Math.round(Math.random()*100);
+//     response.send(number.toString());
+// })
+
+
+// //http request 2
+// exports.tohpvk = functions.https.onRequest((request,response) =>{
+
+//     response.redirect('https://hpvk.com');
+// })
+
+// //http callable function
+// exports.sayHello = functions.https.onCall((data,context) =>{
+
+//     const name = data.name;
+//     return `hello, $(name)`;
+// })
+
+//auth trigger
+exports.newUserSignup = functions.auth.user().onCreate(user => {
+    console.log('user created', user.email,user.uid);
 })
 
-
-//http request 2
-exports.tohpvk = functions.https.onRequest((request,response) =>{
-
-    response.redirect('https://hpvk.com');
-})
-
-//http callable function
-exports.sayHello = functions.https.onCall((data,context) =>{
-
-    const name = data.name;
-    return `hello, $(name)`;
+//auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+    console.log('user deleted', user.email,user.uid);
 })
