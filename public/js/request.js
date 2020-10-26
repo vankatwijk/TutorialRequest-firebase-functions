1bb0c7db0390c4c42388e3f469d40af7bc48a57c
vankatwijk <hendrikus.hpvk@gmail.com>
2020-10-26 10:59:37 +0100

    adding vue.js to manage the list

--- public/js/request.js
+++ public/js/request.js
@@ -1,17 +1,20 @@
-const ref = firebase.firestore().collection('requests');
+var app = new Vue({
+    el: '#app',
+    data: {
+      requests: []
+    },
+    mounted() {
 
-ref.onSnapshot(snapshot => {
-  // console.log(snapshot);
+        const ref = firebase.firestore().collection('requests');
+        ref.onSnapshot(snapshot => {
+            // console.log(snapshot);
 
-  let requests = [];
-  snapshot.forEach(doc => {
-    requests.push({...doc.data(), id: doc.id});
-  });
-  // console.log(requests);
-
-  let html = '';
-  requests.forEach(request => {
-    html += `<li>${request.text}</li>`
-  });
-  document.querySelector('ul').innerHTML = html;
-});
+            let requests = [];
+            snapshot.forEach(doc => {
+                requests.push({...doc.data(), id: doc.id});
+            });
+            // console.log(requests);
+            this.requests = requests;
+        });
+    },
+})
