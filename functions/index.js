49028df1fdbb1c3f51ef930695a382aab0d5e801
vankatwijk <hendrikus.hpvk@gmail.com>
2020-10-26 12:41:11 +0100

    async awaid convertion

--- functions/index.js
+++ functions/index.js
@@ -70,7 +70,44 @@ exports.addRequest = functions.https.onCall((data, context) => {
   });
 
 // upvote callable function
-exports.upvote = functions.https.onCall((data, context) => {
+// exports.upvote = functions.https.onCall((data, context) => {
+//   // check auth state
+//   if (!context.auth) {
+//     throw new functions.https.HttpsError(
+//       'unauthenticated', 
+//       'only authenticated users can vote up requests'
+//     );
+//   }
+//   // get refs for user doc & request doc
+//   const user = admin.firestore().collection('users').doc(context.auth.uid);
+//   const request = admin.firestore().collection('requests').doc(data.id);
+
+//   return user.get().then(doc => {
+//     // check thew user hasn't already upvoted
+//     if(doc.data().upvotedOn.includes(data.id)){
+//       throw new functions.https.HttpsError(
+//         'failed-precondition', 
+//         'You can only vote something up once'
+//       );
+//     }
+
+//     // update the array in user document
+//     return user.update({
+//       upvotedOn: [...doc.data().upvotedOn, data.id]
+//     })
+//     .then(() => {
+//       // update the votes on the request
+//       return request.update({
+//         upvotes: admin.firestore.FieldValue.increment(1)
+//       });
+//     });
+
+//   });
+// });
+
+//convert to async await
+// upvote callable function
+exports.upvote = functions.https.onCall(async (data, context) => {
   // check auth state
   if (!context.auth) {
     throw new functions.https.HttpsError(
@@ -82,25 +119,23 @@ exports.upvote = functions.https.onCall((data, context) => {
   const user = admin.firestore().collection('users').doc(context.auth.uid);
   const request = admin.firestore().collection('requests').doc(data.id);
 
-  return user.get().then(doc => {
-    // check thew user hasn't already upvoted
-    if(doc.data().upvotedOn.includes(data.id)){
-      throw new functions.https.HttpsError(
-        'failed-precondition', 
-        'You can only vote something up once'
-      );
-    }
+  const doc = await user.get();
+  // check thew user hasn't already upvoted
+  if(doc.data().upvotedOn.includes(data.id)){
+    throw new functions.https.HttpsError(
+      'failed-precondition', 
+      'You can only vote something up once'
+    );
+  }
 
-    // update the array in user document
-    return user.update({
-      upvotedOn: [...doc.data().upvotedOn, data.id]
-    })
-    .then(() => {
-      // update the votes on the request
-      return request.update({
-        upvotes: admin.firestore.FieldValue.increment(1)
-      });
-    });
-
+  // update the array in user document
+  await user.update({
+    upvotedOn: [...doc.data().upvotedOn, data.id]
   });
+  
+  // update the votes on the request
+  return request.update({
+    upvotes: admin.firestore.FieldValue.increment(1)
+  });
+
 });
